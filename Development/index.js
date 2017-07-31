c7804e8e93083028b5cf1a406f62d1e1ea07b6f5
yarik-vv <yarik.vashchuk@gmail.com>
2017-08-01 00:21:58 +0300

    created week slider

--- Development/index.js
+++ Development/index.js
@@ -1,3 +1,4 @@
 import fonts from './fonts/fonts.css';
 import main from './css/main.css';
 import header from './css/header.css';
+import header from './css/calendar.css';
--- Production/main.js
+++ Production/main.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -86,16 +86,25 @@
 
 /***/ }),
 /* 3 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 4 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__fonts_fonts_css__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__fonts_fonts_css__ = __webpack_require__(3);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__fonts_fonts_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__fonts_fonts_css__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__css_main_css__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__css_main_css__ = __webpack_require__(2);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__css_main_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__css_main_css__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__css_header_css__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__css_header_css__ = __webpack_require__(1);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__css_header_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2__css_header_css__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__css_calendar_css__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__css_calendar_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3__css_calendar_css__);
+
 
 
 
